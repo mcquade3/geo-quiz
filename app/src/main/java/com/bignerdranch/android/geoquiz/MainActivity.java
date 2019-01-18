f72a357cdcfff056560c090dd471484e108f842d
McQuade <mcquade3@b132-21.upb.pitt.edu>
2019-01-18 14:44:24 -0500

    Add gravity to toasts

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+				Toast Message = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+				Message.setGravity(Gravity.TOP,0,0);
+				Message.show();
 			}
 		});
 
@@ -30,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+				Toast Message = Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+				Message.setGravity(Gravity.TOP,0,0);
+				Message.show();
 			}
 		});
 	}
